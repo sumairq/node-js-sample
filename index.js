437fe88440cbbddab774f05021668f5ad14f0e55
Sumair Qaisar <sumairqaisar992@gmail.com>
2022-11-06 20:22:26 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 3000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('Hello World! It is the main domain here!')
 })
 
 app.listen(app.get('port'), function() {
